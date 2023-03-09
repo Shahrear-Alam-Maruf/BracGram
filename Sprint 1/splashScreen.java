a724602ae6c05d5cbbc63cb367801360db60ca39
Shahrear-Alam-Maruf <111197877+Shahrear-Alam-Maruf@users.noreply.github.com>
2023-03-09 13:31:00 +0600

    Create splashScreen.java

---
+++ Sprint 1/splashScreen.java
@@ -0,0 +1,31 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.facegram2.R;
+import com.example.facegram2.login.login;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), login.class));
+                finish();
+            }
+        }, 2500);
+    }
+}
